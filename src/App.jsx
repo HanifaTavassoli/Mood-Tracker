56f8397a172bad0abbd7efe879bb97d5a5c0565c
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-10-30 06:56:27 +0430

    Setup routes for Home, SignUp, Login, and MoodTracker pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import MoodTracker from "./pages/MoodTracker";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-red-500">Hello React</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/moodtracker" element={<MoodTracker />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
