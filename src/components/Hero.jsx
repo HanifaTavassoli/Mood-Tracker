5087843803e027d179a7f4771eb2b1a69df91781
unknown <hanifatavassoli@gmail.com>
2025-11-01 06:34:18 +0430

    make Hero section full responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,58 +1,44 @@
 import heroImg from "../assets/images/hero-img.jpg";
 import { Link } from "react-router-dom";
+
 function Hero() {
   return (
     <>
-      <div className="pt-[7rem] bg-calm-blue font-montserrat">
-        {/* <div className="flex lg:flex-row items-center justify-center gap-10 border ">
-          <div className="">
-            <h1 className="font-bold text-3xl mb-3">
+      <div className="pt-[5rem] bg-calm-blue font-montserrat">
+        <div className=" flex flex-col lg:flex-row items-center justify-center lg:justify-around gap-10 max-w-7xl mx-auto px-4 md:px-6 pt-5 pb-0">
+    
+          <div className="text-center lg:text-left w-full lg:w-2/5 flex flex-col gap-4">
+            <h1 className="font-bold text-xl md:text-2xl lg:text-3xl mb-4">
               Track Your Mood, Improve Your Life with AI!
             </h1>
-            <p className="text-lg mb-5">
-              Personalized AI suggestions to elevate your mental well-being. Get
-              personalized AI suggestions designed to improve your mental
-              well-being, offering tailored activities and strategies to boost
-              balance and focus.
-            </p>
-            <Link
-              to="/signup"
-              className="bg-white px-3 py-2 rounded-sm border-gray-300"
-            >
-              Get Started
-            </Link>
-          </div>
-          <div className="">
-            <img src={heroImg} alt="hero img" className="w-[700px]" />
-          </div>
-        </div> */}
-        <div className="flex flex-col lg:flex-row items-center justify-around gap-10 w-7xl mx-auto py-6">
-          <div className="text-center flex flex-col lg:text-left w-2/3">
-            <h1 className="font-bold text-5xl mb-4">
-              Track Your Mood, Improve Your Life with AI!
-            </h1>
-            <p className="text-lg mb-5">
+            <p className="text-base md:text-md lg:text-lg mb-5 text-gray-700">
               Personalized AI suggestions to elevate your mental well-being. Get
               tailored activities and strategies to boost balance, focus, and
               overall mental wellness.
             </p>
             <Link
-              to="/signup"
-              className=" text-white px-6 py-3 w-fit rounded-sm bg-cyan-600/80 hover:bg-cyan-600 transition duration-200"
+              to="/moodtracker"
+              className="text-white px-4 py-2 w-fit rounded-sm bg-cyan-600/80 hover:bg-cyan-600 transition duration-200 self-center lg:self-start"
             >
-              Get Started
+              Track Your Mood
             </Link>
           </div>
-          <div className=" lg:w-[700px] w-1/3">
+
+          <div className="w-full lg:w-2/5 flex justify-center lg:justify-end">
             <img
               src={heroImg}
               alt="hero img"
-              className="w-full h-auto rounded-lg"
+              className="w-full max-w-md lg:max-w-lg h-auto rounded-lg object-cover"
             />
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+
+      <svg
+        className="w-full"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
         <path
           fill="#bde2ea"
           d="m0 320 48-42.7C96 235 192 149 288 138.7c96-10.7 192 53.3 288 74.6 96 21.7 192-.3 288-26.6 96-26.7 192-58.7 288-80 96-21.7 192-31.7 240-37.4l48-5.3V0H0Z"
