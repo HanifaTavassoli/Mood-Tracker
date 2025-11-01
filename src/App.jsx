b316cf5261d815c01b71299b2e5dc0e86b3f19bf
unknown <hanifatavassoli@gmail.com>
2025-11-01 06:51:21 +0430

    update App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import MoodTracker from "./pages/MoodTracker";
+import Profile from "./pages/Profile";
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <BrowserRouter>
@@ -15,6 +16,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/moodtracker" element={<MoodTracker />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
